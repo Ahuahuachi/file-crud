f9e3601c708aac377063dc9a7ece47c0dbe05edc
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2021-07-30 20:05:50 -0500

    Add async await

--- promises/muro-promises.js
+++ promises/muro-promises.js
@@ -1,3 +1,8 @@
+// DRY
+// Dont Repeat Yourself
+
+const tiempoDeEspera = 5000;
+
 function construir(muro) {
   // Resultado de funcion
   return new Promise((resolve, reject) => {
@@ -10,16 +15,101 @@ function construir(muro) {
         const error = new Error("No se pudo construir");
         reject(error);
       }
-    }, 3000);
+    }, tiempoDeEspera);
   });
 }
 
-const promesaDeconstruccion = construir({});
+function aplanar(muro) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      muro.aplanado = true;
 
-promesaDeconstruccion
-  .then((muroConstruido) => {
-    console.log("muroConstruido:", muroConstruido);
-  })
-  .catch((error) => {
-    console.error("Ocurrio un error:", error);
+      if (muro.aplanado) {
+        resolve(muro);
+      } else {
+        const error = new Error("No se pudo aplanar");
+        reject(error);
+      }
+    }, tiempoDeEspera);
   });
+}
+
+function pintar(muro) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      muro.pintado = true;
+
+      if (muro.pintado) {
+        resolve(muro);
+      } else {
+        const error = new Error("No se pudo pintado");
+        reject(error);
+      }
+    }, tiempoDeEspera);
+  });
+}
+
+async function crearMuro() {
+  const muro = await construir({});
+  console.log("muroConstruido:", muro);
+
+  const muroAplanado = await aplanar(muro);
+  console.log("muroAplanado:", muroAplanado);
+
+  const muroPintado = await pintar(muroAplanado);
+
+  console.log("muroPintado: ", muroPintado);
+}
+
+crearMuro();
+console.log("Esto es despues de crear muro");
+
+// const promesaDeconstruccion = construir({});
+
+// console.log("promesaDeconsttruccion:", promesaDeconstruccion);
+
+// promesaDeconstruccion
+//   .then((muroConstruido) => {
+//     let promesaDeAplanado = aplanar(muroConstruido);
+//     console.log("muroConstruido:", muroConstruido);
+
+//     promesaDeAplanado
+//       .then((muroAplanado) => {
+//         console.log("muroAplanado:", muroAplanado);
+
+//         let promesaDePintado = pintar(muroAplanado);
+
+//         promesaDePintado
+//           .then((muroPintado) => {
+//             console.log("muroPintado:", muroPintado);
+//           })
+//           .catch((error) => {
+//             console.error(error);
+//           });
+//       })
+//       .catch((error) => {
+//         console.log("Ocurrio un error", error);
+//       });
+//   })
+//   .catch((error) => {
+//     console.error("Ocurrio un error:", error);
+//   });
+
+// completarMuro();
+
+// const promesaDeConstruir = construir({});
+// promesaDeConstruir
+//   .then((muro) => {
+//     console.log("muro construido: ", muro);
+//     return aplanar(muro);
+//   })
+//   .then((muro) => {
+//     console.log("muro aplanado: ", muro);
+//     return pintar(muro);
+//   })
+//   .then((muro) => {
+//     console.log("muro pintado: ", muro);
+//   })
+//   .catch((error) => {
+//     console.log("ocurrio el error: ", error);
+//   });
